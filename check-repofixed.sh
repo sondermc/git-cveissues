9c22b5e6569378b3d690e28163215c0427a61193
Chris <chris.sondermeijer@gmail.com>
2023-01-26 11:54:03 +0100

    formatted output

--- check-repofixed.sh
+++ check-repofixed.sh
@@ -4,6 +4,17 @@ DESCRIPTION="vulnerabilities, CVE-2022-41903, and CVE-2022-23521, that affect ve
 
 for distro in $(cat input.txt)
 do
-  printf "$distro: "
-  podman run --rm ${distro}
+  RAWGITVERSION=$(docker run --rm ${distro})
+  GITVERSION=$(echo ${RAWGITVERSION} | cut -d' ' -f 3)
+  SEMVERMAJOR=$(echo ${GITVERSION} | cut -d. -f 1)
+  SEMVERMINOR=$(echo ${GITVERSION} | cut -d. -f 2)
+  SEMVERPATCH=$(echo ${GITVERSION} | cut -d. -f 3)
+  if [[ ${SEMVERMAJOR} -ge "2" && ${SEMVERMINOR} -ge "39" && ${SEMVERPATCH} -ge "1" ]]
+  then
+    MESSAGE="[OK]"
+  else
+    MESSAGE="[WARNING]"
+  fi
+  
+  printf "${MESSAGE} ${distro}: ${RAWGITVERSION}\n"
 done
