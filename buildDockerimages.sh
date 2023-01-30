8fe9a8007d62f39288b44786fe2c95041d869428
Chris <chris.sondermeijer@gmail.com>
2023-01-30 14:45:15 +0100

    parallel and suppress messages

--- buildDockerimages.sh
+++ buildDockerimages.sh
@@ -2,6 +2,10 @@
 
 DESCRIPTION="vulnerabilities, CVE-2022-41903, and CVE-2022-23521, that affect versions 2.39 and older. Git for Windows was also patched to address an additional, Windows-specific issue known as CVE-2022-41953."
 
+num_procs=0
+RUNC=podman
+MAX=2
+
 build() {
   case $1 in
     alpine-git) DOCKERFILE=Dockerfile.alpine ;;
@@ -31,9 +35,41 @@ build() {
   esac
 }
 
-for distro in $(cat input.txt) 
-do
-  build ${distro}
-  echo Building: ${distro} using Dockerfile: ${DOCKERFILE}
-  time docker build -q -t ${distro} -f ${DOCKERFILE} .
-done
+get_numprocs() {
+  num_procs=$(ps --no-headers -o pid,tty,stat,cmd -C ${RUNC} | wc -l)
+}
+
+count_procs() {
+  get_numprocs
+  until [ "${num_procs}" -le "${MAX}" ]
+  do
+    # echo waiting for number of buildprocesses: ${num_procs} to be less then ${MAX} 
+    sleep 1 ; get_numprocs
+  done
+}
+
+wait_for_last() {
+  get_numprocs
+  until [ ${num_procs} -eq 0 ]
+  do
+    # echo waiting for last containerimage to finish building. ${num_procs} to go
+    sleep 5 ; get_numprocs
+  done
+}
+
+build_image() {
+  for distro in $(cat input.txt) 
+  do
+    build ${distro}
+    echo Building: ${distro} using Dockerfile: ${DOCKERFILE}
+    count_procs
+    ${RUNC} build -t ${distro} -f ${DOCKERFILE} --no-cache --progress=plain . &>/tmp/${distro}.log &
+  done
+}
+
+main() {
+  build_image
+  wait_for_last
+}
+
+main
