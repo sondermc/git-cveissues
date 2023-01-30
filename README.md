d82d21478197734bf7e23593cf2eca95d096bafb
Chris <chris.sondermeijer@gmail.com>
2023-01-30 09:50:43 +0100

    added sles15, gentoo. suppress some output

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
-DESCRIPTION="vulnerabilities, CVE-2022-41903, and CVE-2022-23521, that affect versions 2.39 and older. Git for Windows was also patched to address an additional, Windows-specific issue known as CVE-2022-41953."
+# DESCRIPTION
+vulnerabilities, CVE-2022-41903, and CVE-2022-23521, that affect versions 2.39 and older. Git for Windows was also patched to address an additional, Windows-specific issue known as CVE-2022-41953.
 
 These scripts:
 - build distribution containers and installs the git version provided
 - displays the version
 
+# NOTE
+The building of git on Gentoo takes a long time. Be patient. 
+
 # BUILD
 ```bash
-./buildDockerimages.sh
+time ./buildDockerimages.sh
 ```
    
 # RUN
--- buildDockerimages.sh
+++ buildDockerimages.sh
@@ -4,33 +4,36 @@ DESCRIPTION="vulnerabilities, CVE-2022-41903, and CVE-2022-23521, that affect ve
 
 build() {
   case $1 in
+    alpine-git) DOCKERFILE=Dockerfile.alpine ;;
+    archlinux-git) DOCKERFILE=Dockerfile.archlinux ;;
     centos8-git) DOCKERFILE=Dockerfile.CentOS8 ;;
     centos9-git) DOCKERFILE=Dockerfile.CentOS9 ;;
-    rhel7-git) DOCKERFILE=Dockerfile.rhel7 ;;
-    rhel8-git) DOCKERFILE=Dockerfile.rhel8 ;;
-    rhel9-git) DOCKERFILE=Dockerfile.rhel9 ;;
-    ubuntulunar-git) DOCKERFILE=Dockerfile.ubuntulunar ;;
-    ubuntukinetic-git) DOCKERFILE=Dockerfile.ubuntukinetic ;;
-    ubuntujammy-git) DOCKERFILE=Dockerfile.ubuntujammy ;;
-    ubuntufocal-git) DOCKERFILE=Dockerfile.ubuntufocal ;;
-    ubuntubionic-git) DOCKERFILE=Dockerfile.ubuntubionic ;;
-    ubuntuxenial-git) DOCKERFILE=Dockerfile.ubuntuxenial ;;
-    ubuntutrusty-git) DOCKERFILE=Dockerfile.ubuntutrusty ;;
-    ubuntulunar-git) DOCKERFILE=Dockerfile.ubuntulunar ;;
-    alpine-git) DOCKERFILE=Dockerfile.alpine ;;
+    debianstable-git) DOCKERFILE=Dockerfile.debianstable ;;
+    debiantesting-git) DOCKERFILE=Dockerfile.debiantesting ;;    
+    debianunstable-git) DOCKERFILE=Dockerfile.debianunstable ;;
     fedora34-git) DOCKERFILE=Dockerfile.fedora34 ;;
     fedora35-git) DOCKERFILE=Dockerfile.fedora35 ;;
     fedora36-git) DOCKERFILE=Dockerfile.fedora36 ;;
     fedora37-git) DOCKERFILE=Dockerfile.fedora37 ;;
-    archlinux-git) DOCKERFILE=Dockerfile.archlinux ;;
-    debianstable-git) DOCKERFILE=Dockerfile.debianstable ;;
-    debiantesting-git) DOCKERFILE=Dockerfile.debiantesting ;;    
-    debianunstable-git) DOCKERFILE=Dockerfile.debianunstable ;;
+    gentoo-git) DOCKERFILE=Dockerfile.gentoo ;;
+    rhel7-git) DOCKERFILE=Dockerfile.rhel7 ;;
+    rhel8-git) DOCKERFILE=Dockerfile.rhel8 ;;
+    rhel9-git) DOCKERFILE=Dockerfile.rhel9 ;;
+    sles15-git) DOCKERFILE=Dockerfile.sles15 ;;
+    ubuntubionic-git) DOCKERFILE=Dockerfile.ubuntubionic ;;
+    ubuntufocal-git) DOCKERFILE=Dockerfile.ubuntufocal ;;
+    ubuntujammy-git) DOCKERFILE=Dockerfile.ubuntujammy ;;
+    ubuntukinetic-git) DOCKERFILE=Dockerfile.ubuntukinetic ;;
+    ubuntulunar-git) DOCKERFILE=Dockerfile.ubuntulunar ;;
+    ubuntulunar-git) DOCKERFILE=Dockerfile.ubuntulunar ;;
+    ubuntutrusty-git) DOCKERFILE=Dockerfile.ubuntutrusty ;;
+    ubuntuxenial-git) DOCKERFILE=Dockerfile.ubuntuxenial ;;
   esac
 }
 
 for distro in $(cat input.txt) 
 do
-  build $distro
-  docker build -t ${distro} -f ${DOCKERFILE} .
+  build ${distro}
+  echo Building: ${distro} using Dockerfile: ${DOCKERFILE}
+  time docker build -q -t ${distro} -f ${DOCKERFILE} .
 done
