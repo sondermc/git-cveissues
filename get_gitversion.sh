e750d28d2955174eb961aafb831cc4b6a62b30cf
Chris <chris.sondermeijer@gmail.com>
2023-01-31 08:52:16 +0100

    removed messages due to ubuntu patching see: https://ubuntu.com/security/notices/USN-5810-1
    for more info

--- get_gitversion.sh
+++ get_gitversion.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 DESCRIPTION="vulnerabilities, CVE-2022-41903, and CVE-2022-23521, that affect versions 2.39 and older. Git for Windows was also patched to address an additional, Windows-specific issue known as CVE-2022-41953."
 
+printf "== NOTE ==========================================================================================\n"
+printf "Ubuntu released this notification:\n\n"
+printf "    https://ubuntu.com/security/notices/USN-5810-1\n\n"
+printf "Conditional checking in bash could result in unreadable scripts.\n"
+printf "== NOTE ==========================================================================================\n"
+
 for distro in $(cat input.txt)
 do
   RAWGITVERSION=$(docker run --rm ${distro})
@@ -8,12 +14,5 @@ do
   SEMVERMAJOR=$(echo ${GITVERSION} | cut -d. -f 1)
   SEMVERMINOR=$(echo ${GITVERSION} | cut -d. -f 2)
   SEMVERPATCH=$(echo ${GITVERSION} | cut -d. -f 3)
-  if [[ ${SEMVERMAJOR} -ge "2" && ${SEMVERMINOR} -ge "39" && ${SEMVERPATCH} -ge "1" ]]
-  then
-    MESSAGE="[OK]"
-  else
-    MESSAGE="[WARNING]"
-  fi
-  
-  printf "${MESSAGE} ${distro}: ${RAWGITVERSION}\n"
+  printf "${distro}: ${RAWGITVERSION}\n"
 done
